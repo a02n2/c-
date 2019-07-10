a425a529bba46e3651b4adbe2367f503204f07b7
a02n2 <49223782+a02n2@users.noreply.github.com>
2019-07-10 10:40:10 +0900

    Create remort.c

---
+++ remort.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<string.h>
+#pragma warning(disable: 4996)
+
+void main(void) {
+	FILE *fp;
+	char sBuf[1000][261];	//ファイルの中身
+	char key[256];	//検索キー
+	char *adr;		//検索キーと合致するデータのアドレス
+
+	/*ファイルを開いて中身を配列に格納*/
+	if ((fp = fopen("data.txt", "r")) == NULL) {
+		printf("ファイルのオープンに失敗しました。");
+		return;
+	}
+
+	for (int i = 0; i < 1000; i++) {
+		fgets(&(sBuf[i][0]), 261, fp);
+	}
+	fclose(fp);
+
+	while (1) {
+		printf("3桁の数字を入力して下さい[終了/EXIT]:");
+		scanf("%s", key);
+
+		/*EXITが入力されると終了*/
+		if (strcmp(key, "EXIT") == 0) {
+			return;
+		}
+
+		/*検索キーの長さが3でないとデータなし*/
+		if (strlen(key) != 3) {
+			printf("[検索結果]:該当データなし\n");
+			continue;
+		}
+		
+		/*検索キーと合致するデータを探す*/
+		for (int i = 0; i < 1000; i++) {
+			adr = strstr(sBuf[i], key);
+			if (adr != NULL) {
+				printf("[該当データ]:%s", &(sBuf[i][4]));
+				break;
+			}
+		}
+		if (adr == NULL) {
+			printf("[検索結果]:該当データなし\n");
+		}
+	}
+}
